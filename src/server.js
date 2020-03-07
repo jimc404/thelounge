d25714375df35e052f20331189cd84b92adc935f
Richard Lewis <richrd.lewis@gmail.com>
2020-03-07 14:56:50 +0200

    Search improvements.

--- src/server.js
+++ src/server.js
@@ -595,12 +595,6 @@ function initializeClient(socket, client, token, lastMessage, openChannel) {
 
 	socket.on("sessions:get", sendSessionList);
 
-	socket.on("search", (query) => {
-		client.search(query).then((results) => {
-			socket.emit("search:results", results);
-		});
-	});
-
 	if (!Helper.config.public) {
 		socket.on("setting:set", (newSetting) => {
 			if (!newSetting || typeof newSetting !== "object") {
@@ -647,6 +641,12 @@ function initializeClient(socket, client, token, lastMessage, openChannel) {
 
 			const clientSettings = client.config.clientSettings;
 			socket.emit("setting:all", clientSettings);
+		});
+
+		socket.on("search", (query) => {
+			client.search(query).then((results) => {
+				socket.emit("search:results", results);
+			});
 		});
 	}
 
